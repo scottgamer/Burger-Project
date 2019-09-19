8af0975e3b9a133776176715b0240eabea5c4eea
Richard Munoz <richosojason@msn.com>
2019-09-18 23:17:00 -0500

    added layout component

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -423,7 +423,7 @@ module.exports = function(webpackEnv) {
                 sourceMap: isEnvProduction && shouldUseSourceMap,
                 // Add Css Loader support
                 modules: true,
-                localIdentName: '[name]__[local]__[base:64:5]',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
