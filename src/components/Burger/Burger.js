e209461b030ecf67b78c3c39ed1c1e34ab1a59f3
Richard Munoz <richosojason@msn.com>
2019-09-21 00:13:34 -0500

    build controls added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,28 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  // number of ingredients for each type of ingredient
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])]
+        .map((_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
-      <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
-      <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
